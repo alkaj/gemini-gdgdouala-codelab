cfd690ca10d258f7d218a80935d1c4c9f29cff75
Junior Kamla <alkaj09@gmail.com>
2024-04-06 10:31:16 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    dependencies {
+        classpath(libs.secrets.gradle.plugin)
+    }
+}
+
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.gradleSecrets) apply false
+}
