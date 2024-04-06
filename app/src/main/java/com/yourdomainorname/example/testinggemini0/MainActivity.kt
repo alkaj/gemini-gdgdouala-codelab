81474a5b0c6db6d79f942496dd3d38da7d401025
Junior Kamla <alkaj09@gmail.com>
2024-04-06 20:03:59 +0100

    Adding a thin layer of polishment

--- app/src/main/java/com/yourdomainorname/example/testinggemini0/MainActivity.kt
+++ app/src/main/java/com/yourdomainorname/example/testinggemini0/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.yourdomainorname.example.testinggemini0
 
-import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.MaterialTheme.shapes
 import androidx.compose.material3.Surface
@@ -28,14 +29,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.ai.client.generativeai.GenerativeModel
 import com.yourdomainorname.example.testinggemini0.ui.theme.TestingGemini0Theme
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity()
@@ -50,7 +50,7 @@ class MainActivity : ComponentActivity()
         )
         setContent {
             val prompt = remember { mutableStateOf("")}
-            val responsesList = remember { SnapshotStateList<String>() }
+            val responsesList = remember { SnapshotStateList< Pair<Int, String>>() }
             val compositionScope = rememberCoroutineScope()
             TestingGemini0Theme {
                 // A surface container using the 'background' color from the theme
@@ -62,7 +62,8 @@ class MainActivity : ComponentActivity()
                         compositionScope.launch {
                             try {
                                 val response = model.generateContent(it)
-                                val finalResponse = if (null == response.text) "Pas de restour!" else "${prompt.value}:\n ${response.text}"
+                                val finalResponse = Pair(2, if (null == response.text) "Pas de restour!" else response.text!!)
+                                responsesList.add(Pair(1, prompt.value))
                                 responsesList.add(finalResponse)
                                 prompt.value = ""
                             } catch (e: Exception) {
@@ -78,35 +79,54 @@ class MainActivity : ComponentActivity()
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier, prompt: MutableState<String> = remember { mutableStateOf("") }, responses: SnapshotStateList<String>, onSend: (promptContent: String) -> Unit = {})
+fun Greeting(modifier: Modifier = Modifier, prompt: MutableState<String> = remember { mutableStateOf("") }, responses: SnapshotStateList<Pair<Int, String>>, onSend: (promptContent: String) -> Unit = {})
 {
-    Column(
-        verticalArrangement = Arrangement.Bottom,
+    LazyColumn(
+        reverseLayout = true,
         modifier = Modifier
             .fillMaxSize()
             .padding(horizontal = 16.dp)) {
-        LazyColumn(
-            verticalArrangement = Arrangement.Bottom,
-            modifier = Modifier.fillMaxSize(.8f)) {
-            item {
-                responses.forEach {
-                    Text(text = it, modifier = Modifier.background(colorScheme.onBackground.copy(.1f), shape = shapes.medium).padding(vertical = 2.dp, horizontal = 4.dp))
-                    Spacer(modifier = Modifier.height(16.dp))
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxHeight()) {
+                TextField(
+                    value = prompt.value,
+                    maxLines = 1,
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Go
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onGo = {onSend(prompt.value)}
+                    ),
+                    shape = shapes.medium,
+                    onValueChange = {prompt.value = it},
+                    modifier = Modifier.fillMaxWidth(.7f)
+                )
+                Spacer(modifier = Modifier.width(16.dp))
+                Button(onClick = { onSend(prompt.value) }) {
+                    Text(text = "Send")
                 }
             }
+            Spacer(modifier = Modifier.height(24.dp))
         }
-        Spacer(modifier = Modifier.height(24.dp))
-        Row {
-            TextField(
-                value = prompt.value,
-                onValueChange = {prompt.value = it}
-            )
-            Spacer(modifier = Modifier.width(16.dp))
-            Button(onClick = { onSend(prompt.value) }) {
-                Text(text = "Send")
-            }
+        items(responses.size) {
+            val response = responses.reversed()[it]
+            if (response.first == 1)
+            {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End) {
+                    Text(text = response.second, modifier = Modifier
+                        .background(colorScheme.onBackground.copy(.1f), shape = shapes.medium)
+                        .padding(vertical = 4.dp, horizontal = 8.dp))
+                }
+            } else Text(text = response.second, modifier = Modifier
+                .background(colorScheme.primary.copy(.1f), shape = shapes.medium)
+                .padding(vertical = 4.dp, horizontal = 8.dp))
+            Spacer(modifier = Modifier.height(16.dp))
         }
-        Spacer(modifier = Modifier.height(24.dp))
     }
 }
 
@@ -115,10 +135,9 @@ fun Greeting(modifier: Modifier = Modifier, prompt: MutableState<String> = remem
 fun GreetingPreview()
 {
     TestingGemini0Theme {
-
-        val responses = SnapshotStateList<String>()
-        responses.add("Ceci est un test")
-        responses.add("Ceci est un autre test")
+        val responses = SnapshotStateList<Pair<Int, String>>()
+        responses.add(Pair(1, "C'est quoi ca?"))
+        responses.add(Pair(2, "C'est un test"))
         Greeting(responses = responses)
     }
 }
